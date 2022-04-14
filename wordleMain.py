e465aff5d46a024147cc507ed4c841c5f3f68618
Chloe <chloe.lias@smes.org>
2022-04-14 14:10:25 -0700

    fkjlakdj;f

--- wordleMain.py
+++ wordleMain.py
@@ -2,22 +2,29 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import random as rd
+import colorama as color
+
+funTime = False
 
 # word list: awful, clumpy, butts, coagulant, poopy, cruel
 
-words = ['awful', 'clumpy', 'butts', 'coagulate', 'poopy',
-         'unfun', 'ducks', 'panda', "timmy", "TIMMY",
-         "jyang", "liang", "skyla", "adhoc", "antic",
-         'color','blind', 'chole', 'chloe', 'cruel', 'vegan',
-        'idaho', "Lplus", "ratio", "chaos", "farts",
-         "amoeba", "among", "edwin"]
+funWords = ['awful', 'clumpy', 'butts', 'coagulate', 'poopy',
+            'unfun', 'ducks', 'panda', "timmy", "TIMMY",
+            "jyang", "liang", "skyla", "adhoc", "antic",
+            'color', 'blind', 'chole', 'color', 'blind', 'chloe', 'cruel', 'vegan',
+            'idaho', "Lplus", "ratio", "chaos", "farts",
+            "amoeba", "among", "edwin", "dance"]
 
 wordsReal = pd.read_csv("Wordle.csv")
 wordsReal = wordsReal["validWordleAnswer"]
 wordsReal = wordsReal.dropna()
-print(wordsReal)
+# print(wordsReal)
 
 choiceIndex = rd.randint(0, len(wordsReal))
-choiceWord = wordsReal[choiceIndex]
+if (funTime):
+    choiceIndex = rd.randint(0, len(funWords))
+    choiceWords = funWords[choiceIndex]
+else:
+    choiceWord = wordsReal[choiceIndex]
 
 print(choiceWord)
