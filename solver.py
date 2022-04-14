1b42b5c5a8baaee5a44b073115ac63637ce4eff9
Chloe <chloe.lias@smes.org>
2022-04-14 14:11:01 -0700

    Changes by andrew

--- solver.py
+++ solver.py
@@ -1,18 +1,67 @@
 import pandas as pd
-import numpy as np
 
-text_file = pd.read_csv()
-wordArray = text_file.read().split(',')
+# text_file = open("words.txt", "r")
+# wordArray = text_file.read().split('\n')
 
-actualWord = "which" #testing with this word
+wordArray = pd.read_csv("Wordle.csv")
+wordArray = wordArray["validWordleAnswer"]
+
+
+print(wordArray)
+
+#word is swagg
+# goops
+# g - [0]
+
+
+actualWord = "which"
 startingWord = "adieu"
+backupWord = "golly"
 currGuess = startingWord
 
-correctLetters = ["","","","",""]
+correctLetters = ['','','','','']
+incorrectLetters = [[]]
+lettersNotIn = []
 
-#this will be the for loop to check if the words finished
-#while actualWord != "".join(correctLetters);
+# while actualWord != "".join(correctLetters):
+for i in range(len(currGuess)):
+    currLetter = currGuess[i]
+    for j in range(len(actualWord)):
+        wordLetter = actualWord[j]
+        letterTwice = False
+        if (currLetter == wordLetter and i != j):
+            print(currLetter + " is in the word but not in the right place")
+            #
+            incorrectLetters.append(currLetter)
+            incorrectLetters[currLetter].append(i)
+            letterTwice = True
+        if (currLetter == wordLetter and i == j):
+            print(currLetter + " is in the word and in the right place")
+            correctLetters[i] = currLetter
+            if (currLetter in incorrectLetters and letterTwice == False):
+                incorrectLetters.remove(currLetter)
 
-for l in range(len(currGuess)):
-    if (l in actualWord):
-        print(l)
+    if (currLetter not in correctLetters) and (currLetter not in incorrectLetters):
+        lettersNotIn.append(currLetter)
+#get new guess
+if  len(incorrectLetters) == 5:
+    currGuess = backupWord
+else:
+    #generate new guess based on info
+# for w in wordArray:
+#     # finding words in the wordlist that match the stuff we've already found
+#     # do we need to convert all the strings in wordArray into lists with 5 letters
+#     # does w[i] work if w is a string
+#     for i in range(len(correctLetters)):
+#         if w[i] == correctLetters[i]:
+#             print(w)
+#             #word has letter in correct place
+#         if w[i] in incorrectLetters:
+#             print(w)
+#             #word has letter but in wrong place
+
+print(correctLetters)
+print(incorrectLetters)
+print(lettersNotIn)
+
+
