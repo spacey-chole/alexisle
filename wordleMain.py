3f57a9946c85146e37f93b4d17103dab9d163cdd
DylanMeeks <81591487+DylanMeeks@users.noreply.github.com>
2022-04-19 09:31:08 -0700

    Update wordleMain.py

--- wordleMain.py
+++ wordleMain.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import random as rd
-from colorama import Fore, Back, Style
+from colorama import Fore, Back, Style, init
+init(autoreset=True)
 
 # Joke word or not to joke word
 funTime = False
@@ -19,20 +20,24 @@ funWords = ['awful', 'clumpy', 'butts', 'coagulate', 'poopy',
 # Reads csv file and assigns the column of potential words to an array of words
 wordsReal = pd.read_csv("Wordle.csv")
 wordsReal = wordsReal["validWordleAnswer"]
-# Gets rid of all of the NaN values in the list
+# Gets rid of all the NaN values in the list
 wordsReal = wordsReal.dropna()
 
-# selects a random index to use as the wordle word
-choiceIndex = rd.randint(0, len(wordsReal))
+# list of valid guesses
+validGuess = pd.read_csv("Wordle.csv")
+validGuess = validGuess["validWordleGuess"].dropna()
 
-# If joke words, assigns the guessing word from the joke word list
-# if normal, assign to choiceIndex of choice words
-if funTime:
-    choiceIndex = rd.randint(0, len(funWords))
-    choiceWords = funWords[choiceIndex]
-else:
-    choiceWord = wordsReal[choiceIndex]
-
+# # selects a random index to use as the wordle word
+# choiceIndex = rd.randint(0, len(wordsReal))
+#
+# # If joke words, assigns the guessing word from the joke word list
+# # if normal, assign to choiceIndex of choice words
+# if funTime:
+#     choiceIndex = rd.randint(0, len(funWords))
+#     choiceWords = funWords[choiceIndex]
+# else:
+#     choiceWord = wordsReal[choiceIndex]
+choiceWord = "dummy"
 print(choiceWord)
 
 guessWord = "_____"
@@ -41,27 +46,50 @@ guessWord = "_____"
 numGuess = 0
 
 # while loop, iterates while the guess is wrong and numGuess is less than 5
-while guessWord != choiceWord and numGuess < 5:
+while guessWord != choiceWord and numGuess < 6:
     # prints out your guess word so far
     for char in guessWord:
         print(char, end="")
     print()
 
-    # sets guessword to your input
+    # sets guess-word to your input
     guessWord = input()
+
+    # # check if word is vamlid guess
+    # while guessWord not in validGuess and guessWord not in wordsReal:
+    #     print("Not in word list")
+    #     guessWord = input()
+
+    #
+    correctLet = []
+    for j in choiceWord:
+        correctLet.append(j)
 
     # checks guess, and prints in terminal if the letter is in the right spot (green),
     # if it is in the word but not in the right spot (yellow), or if the letter isn't in the word
     # at the end, increment numGuess
+
+    # indices of guess word that are green/yellow
+    greenIn = []
+    yellowIn = []
+
+    # TODO put green check in seperate for loop to stop interference
+
     for i in guessWord:
         if i == choiceWord[guessWord.index(i)]:
             print(Back.GREEN + i, end="")
-            print(Style.RESET_ALL)
-        elif i in choiceWord:
+            correctLet.remove(i)
+            greenIn.append(guessWord.index(i))
+        elif correctLet.__contains__(i):
             print(Back.YELLOW + i, end="")
-            print(Style.RESET_ALL)
+            correctLet.remove(i)
         else:
             print(i, end="")
         print()
     numGuess += 1
 
+# output messages depending on result
+if guessWord == choiceWord:
+    print("You guessed the word")
+else:
+    print("The word was ", choiceWord)
