b2ec0882bbd23ce1d5313eae0efc5e4152497159
Chloe <chloe.lias@smes.org>
2022-04-22 12:02:21 -0700

    Changes by nadia.momtaz

--- gui.py
+++ gui.py
@@ -21,16 +21,31 @@ xx = np.arange(0, ncols + 1, (wid+inbetween))
 yy = np.arange(0, nrows + 1, (hei+inbetween))
 fig = plt.figure(figsize=(10,10))
 
-ax = plt.subplot(111, aspect = 'equal')
-plt.xlim([-.25,6.25])
-plt.ylim([-0.25,7.5])
+def update_board():
+    # displaying colors
+    for i in range(5):
+        for j in range(6):
+            print(xx[i], yy[j])
+            sq = patches.Rectangle((xx[i], yy[j]), wid, hei, fill=True, color=status[i][j])
+            ax.add_patch(sq)
+    # for i in range(5):
+    # for j in range(6):
+    # ax.text( 3, 3, letters,  fontsize=20)
 
-pat = []
-for i in range(5):
-    for j in range(6):
-        print(xx[i], yy[j])
-        sq = patches.Rectangle((xx[i], yy[j]), wid, hei, fill=True, color = status[i-1][j-1])
-        ax.add_patch(sq)
+    # display keyboard
+    num = len(keyboard[i])
+    thing = ""
+    for i in range(len(keyboard[i])):
+        for v in range(num - 1):
+            xkey = np.arange(0, num, (keywid + inbetween))
+
+            thing += " "
+        # for j in range(i*2-1):
+
+        num -= 1;
+        sq = patches.Rectangle((xkey[i], ykey[j]), keywid, keyhei, fill=True, color="black")
+
+    print(thing)
 
 pc = coll.PatchCollection(pat)
 ax.add_collection(pc)
